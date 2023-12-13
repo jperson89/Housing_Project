5f7e3fe38049f8e502d9c94346dc1878afd15596
jperson89 <146031791+jperson89@users.noreply.github.com>
2023-12-12 21:09:29 -0600

    Delete files.py

--- files.py
+++
@@ -1,5 +0,0 @@
-# These are the files that we are using for our python commands
-
-housingFile = "C:\\Users\\jmper\\PycharmProjects\\HousingProject\\files\\housing-info.csv"
-incomeFile = "C:\\Users\\jmper\\PycharmProjects\\HousingProject\\files\\income-info.csv"
-zipFile = "C:\\Users\\jmper\\PycharmProjects\\HousingProject\\files\\zip-city-county-state.csv"
